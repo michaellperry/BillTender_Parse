62dac300a12820ed274ed579e737564f851cc113
Michael L Perry <michael@qedcode.com>
2013-12-08 12:02:51 -0600

    Added UniqueId

--- BillTender/Budget/Mementos/BillMemento.cs
+++ BillTender/Budget/Mementos/BillMemento.cs
@@ -6,6 +6,7 @@ namespace BillTender.Budget.Mementos
     public class BillMemento
     {
         public string ObjectId { get; set; }
+        public string UniqueId { get; set; }
         public string Payee { get; set; }
         public Frequency Frequency { get; set; }
         public double Amount { get; set; }
